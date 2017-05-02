d2d8562acb710a5a1edc2e06f40d78670839af94
Keping <kw238@duke.edu>
2017-05-02 00:50:15 -0400

    write lp directly to file

--- lp/gen_lp.py
+++ lp/gen_lp.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 import graph_utils
 from functools import reduce
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("inputfile", help="input .net file")
-parser.add_argument("-d", "--digraph", help="convert the input graph to digraph",
+parser.add_argument("--digraph", help="convert the input graph to digraph",
                     action="store_true")
 parser.add_argument("--debug", help="debug mode print out current constraint count",
                     action="store_true")
@@ -20,6 +20,9 @@ parser.add_argument("--debug", help="debug mode print out current constraint cou
 # flow conservation constraints
 # capacity constraints
 
+temp_objective_file = ".temp_objective.txt"
+temp_constraints_file = ".temp_constraints.txt"
+temp_bounds_file = ".temp_bounds.txt"
 
 
 class VarReg():
@@ -36,18 +39,24 @@ class VarReg():
       self.var_dict[(req, edge)] = "x" + str(self.count)
       self.count += 1
       return self.var_dict[(req, edge)]
-  def min_cost_expr(self):
-    return reduce(
-      lambda var1, var2: var1 + " + " + var2,
-      ["{} {}".format(edge.cost, varname)
-       for (req, edge), varname in self.var_dict.items()]
-    )
-  def bounds(self):
-    return reduce(
-      lambda x, y: x + y,
-      ["0 <= {} <= +inf\n".format(varname)
-       for _, varname in self.var_dict.items()]
-    )
+  def write_objective(self, filename):
+    first_var = True
+    with open(filename, 'w') as f:
+      f.write("Minimize\n")
+      for (req, edge), varname in self.var_dict.items():
+        if first_var:
+          f.write("{} {}".format(edge.cost, varname))
+          first_var = False
+        else:
+          f.write(" + {} {}".format(edge.cost, varname))
+      f.write("\n")
+  def write_bounds(self, filename):
+    with open(filename, 'w') as f:
+      f.write("Bounds\n")
+      for _, varname in self.var_dict.items():
+        f.write("0 <= {} <= +inf\n".format(varname))
+      f.write("\n")
+      f.write("End\n")
   def __str__(self):
     res = "\ variable dictionary:\n"
     for req_edge_pair, varname in self.var_dict.items():
@@ -55,26 +64,22 @@ class VarReg():
     return res
 
 class MultiCommMinCostFlowLP():
-  def __init__(self, graph, debug=False):
+  def __init__(self, graph, outfilename, debug=False):
     self.var = VarReg()
     self.debug = debug
+    self.graph = graph
     self.constraint_count = 1
-    self._constraints = "Subject To\n"
-    self._constraints += "\ Bandwidth Constraints\n"
-    self._constraints += self._bandwidth_constraints(graph)
-    self._constraints += "\ Flow Conservation Constraints\n"
-    self._constraints += self._flow_conservation_constraints(graph)
-    self._constraints += "\ Capacity Constraints\n"
-    self._constraints += self._capacity_constraints(graph)
-    self._constraints += "\n"
-    self._objective = "Minimize\n"
-    self._objective += self.var.min_cost_expr()
-    self._objective += "\n\n"
-    self._bounds = "Bounds\n"
-    self._bounds += self.var.bounds()
-    self._bounds += "\n"
-    self._lp_expression = self._objective \
-                          + self._constraints + self._bounds + "End\n\n"
+    self.write_constraints(temp_constraints_file)
+    self.var.write_objective(temp_objective_file)
+    self.var.write_bounds(temp_bounds_file)
+    with open(outfilename, 'w') as fout:
+      for tempfilename in [temp_objective_file, temp_constraints_file, temp_bounds_file]:
+        with open(tempfilename, 'r') as fin:
+            for line in fin:
+                fout.write(line)
+    for tempfilename in [temp_objective_file, temp_constraints_file, temp_bounds_file]:
+      os.remove(tempfilename)
+    print("LP problem written in {}".format(outfilename))
 
   def _count_constraint(self):
     if self.debug:
@@ -92,63 +97,70 @@ class MultiCommMinCostFlowLP():
       [self.var.get(req, edge) for req in reqs]
     )
 
+  def write_constraints(self, filename):
+    with open(filename, 'w') as f:
+      f.write("Subject To\n")
+    self._bandwidth_constraints(self.graph, filename)
+    self._flow_conservation_constraints(self.graph, filename)
+    self._capacity_constraints(self.graph, filename)
   
-  
-  def _bandwidth_constraints(self, graph):
+  def _bandwidth_constraints(self, graph, filename):
     """ # req * # nodes
     """
-    res = ""
-    for req in graph.reqs:
-      if graph.edges_out(req.src):
-        res += "{flow_req_src} >= {req_val}\n".format(
-          flow_req_src = self.flows_r(req, graph.edges_out(req.src)),
-          req_val = req.val
-        )
-        self._count_constraint()
-    return res
-
-  def _flow_conservation_constraints(self, graph):
-    """ # edge * # req
-    """
-    res = ""
-    for node in graph.nodes:
+    with open(filename, 'a') as f:
+      f.write("\\ Requirement (Bandwidth) Constraints\n")
       for req in graph.reqs:
-        if node not in (req.src, req.dst):
-          if graph.edges_in(node):
-            res += self.flows_r(req, graph.edges_in(node))
-          if graph.edges_out(node):
-            res += " - " + self.flows_r(req, graph.edges_out(node), sep=" - ")
-          if res != "":
-            res += " = 0\n"
-            self._count_constraint()
-    return res
+        if graph.edges_out(req.src):
+          res = "{flow_req_src} >= {req_val}\n".format(
+            flow_req_src = self.flows_r(req, graph.edges_out(req.src)),
+            req_val = req.val
+          )
+          f.write(res)
+          self._count_constraint()
 
-  def _capacity_constraints(self, graph):
+  def _flow_conservation_constraints(self, graph, filename):
     """ # edge * # req
     """
-    res = ""
-    for edge in graph.edges:
-      if graph.reqs:
-        res += "{flow_edge} <= {edge_cap}\n".format(
-          flow_edge = self.flows_e(edge, graph.reqs),
-          edge_cap = edge.cap
-        )
-        self._count_constraint()
-    return res
+    with open(filename, 'a') as f:
+      f.write("\\ Flow Conservation Constraints\n")
+      for node in graph.nodes:
+        for req in graph.reqs:
+          if node not in (req.src, req.dst):
+            res = ""
+            if graph.edges_in(node):
+              res += self.flows_r(req, graph.edges_in(node))
+            if graph.edges_out(node):
+              res += " - " + self.flows_r(req, graph.edges_out(node), sep=" - ")
+            if res != "":
+              res += " = 0\n"
+              f.write(res)
+            self._count_constraint()
+
+  def _capacity_constraints(self, graph, filename):
+    """ # edge * # req
+    """
+    with open(filename, 'a') as f:
+      f.write("\\ Capacity Constraints\n")
+      for edge in graph.edges:
+        if graph.reqs:
+          res = "{flow_edge} <= {edge_cap}\n".format(
+            flow_edge = self.flows_e(edge, graph.reqs),
+            edge_cap = edge.cap
+          )
+          f.write(res)
+          self._count_constraint()
 
   @property
   def var_dict(self):
     return self.var
-  @property
-  def lp_expression(self):
-    return self._lp_expression
+
 
 if __name__ == '__main__':
   args = parser.parse_args()
   graph = graph_utils.read_graph(args.inputfile)
   if args.digraph:
     graph = graph_utils.convertToDiGraph(graph)
-  lp = MultiCommMinCostFlowLP(graph, args.debug)
+  lp = MultiCommMinCostFlowLP(graph, args.inputfile+".lp", debug=args.debug)
   if args.debug:
     print(lp.var_dict)
-  print(lp.lp_expression)
+
--- lp_solve_digraph.sh
+++ lp_solve_digraph.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]
     echo "input the .net filename"
 fi
 
-echo "======== The LP ========"
-python3 lp/gen_lp.py $1
-echo "======== Solving the LP ========"
+echo "======== The Digraph LP ========"
+python3 lp/gen_lp.py $1 --digraph
+echo "======== Solving the Digraph LP ========"
 cplex -c "read $1.lp" "optimize" "display solution variables 1-" | tee $1.lp.sol
